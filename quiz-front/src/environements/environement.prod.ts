611a81f8e90de144fc09d1ec5901e17c1c19e9a0
Theotime.Lebreton <theotime.lebreton@gmail.com>
2025-11-05 11:51:00 +0100

    Modification docker compose et environement.prod.ts du front pour prod

--- quiz-front/src/environements/environement.prod.ts
+++ quiz-front/src/environements/environement.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  gatewayUrl: 'http://localhost:8222',
+  gatewayUrl: 'https://qlture.dewal.fr',
   quizApi: '/QUIZ-SERVICE/api',
   userApi: '/USER-SERVICE/api',
   rewardApi: '/REWARD-SERVICE/api',
