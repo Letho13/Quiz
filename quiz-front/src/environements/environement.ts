48000a4de3f476c8f75a8f3b14d41a7d4d8eb9c7
Theotime.Lebreton <theotime.lebreton@gmail.com>
2025-11-06 23:13:03 +0100

    modification env front

--- quiz-front/src/environements/environement.ts
+++ quiz-front/src/environements/environement.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  gatewayUrl: 'http://localhost:8222',
+  gatewayUrl: 'https://qlture.dewal.fr',
+  // gatewayUrl: 'http://localhost:8222',
   quizApi: '/QUIZ-SERVICE/api',
   userApi: '/USER-SERVICE/api',
   rewardApi: '/REWARD-SERVICE/api',
